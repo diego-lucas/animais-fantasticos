0d38c46242351be57c14848cecff428ee4341e82
Diego <diego@beanalytic.com.br>
2020-06-09 19:41:54 -0300

    Anima numeros refatorados

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,30 +1,35 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
-export default function initFetchAnimais(){
-    async function fetchAnimais(url){
-        try{
-            const animaisResponse = await fetch(url);
-            const animaisJSON = await animaisResponse.json();
-            const numerosGrid = document.querySelector(".numeros-grid");
-        
-            animaisJSON.forEach(animal => {
-                const divAnimal = createAnimal(animal);
-                numerosGrid.appendChild(divAnimal);
-            });
-            initAnimaNumeros();
-        } catch(erro){
-            console.log(erro);
-        }
+export default function initFetchAnimais() {
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
+    } catch (erro) {
+      console.log(erro);
     }
-    
-    function createAnimal(animal){
-        const div = document.createElement("div");
-        div.classList.add("numero-animal");
-    
-        div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
-    
-        return div;
-    }
-    
-    fetchAnimais("./animaisapi.json");    
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+
+    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+
+    return div;
+  }
+
+  fetchAnimais("./animaisapi.json");
 }
